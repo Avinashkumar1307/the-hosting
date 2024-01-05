a23d44fc91f4ad339288787cfddf9157dabc7cae
Avinash kumar <avinash2y20@gmail.com>
2024-01-05 17:09:25 +0530

    first commit

---
+++ BackEnd/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+require('./config/database').connect();
+
+// Routes import and mount
+
+const user = require('./routes/user');
+app.use('/api/v1', user);
+const phonepeRoute = require('./routes/phonepeRoute')
+app.use("/api", phonepeRoute);
+app.get("/",(req,res)=>{
+    res.json("hello")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is Started ${PORT}`);
+})
